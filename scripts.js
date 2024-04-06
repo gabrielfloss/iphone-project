32741d91cc4637c8af9af1df237ec6b2d7518502
gabrielfloss <gbgabrielr1@gmail.com>
2024-04-06 15:31:38 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+function trocaCor(cor){
+    const circulo = document.querySelector(".circulo")
+
+    circulo.style.background = cor
+}
+
+function trocaImg(img){
+
+    const iphone = document.querySelector(".iphone-azul")
+     
+    iphone.src = img
+
+    
+}
+
